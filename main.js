3cff58de90f6551b6680c8bb35931638b822b894
Emmanuel Dalyop <108156886+EcodeJR@users.noreply.github.com>
2022-11-24 15:10:19 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ var loader = document.getElementById("loader");
 window.addEventListener("load", function() {
     loader.style.display = "none";
 });
+let open = document.getElementById('open_nav');
+let close = document.getElementById('close_nav');
+let list = document.getElementById('list');
+open.addEventListener('click', () => {
+    list.style.top = '0';
+    list.style.transition = 'all .4s';
+})
+close.addEventListener('click', () => {
+    list.style.top = '-100vh';
+    list.style.transition = 'all .4s';
+})
